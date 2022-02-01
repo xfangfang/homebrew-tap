0c57a8bca8b28048a5edfc98048bb3bcdda1be7c
xfangfang <2553041586@qq.com>
2022-02-01 20:14:59 +0800

    Update mpv.rb

--- mpv.rb
+++ mpv.rb
@@ -1,46 +1,26 @@
 class Mpv < Formula
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io"
-  url "https://github.com/mpv-player/mpv/archive/v0.33.1.tar.gz"
-  sha256 "100a116b9f23bdcda3a596e9f26be3a69f166a4f1d00910d1789b6571c46f3a9"
+  url "https://github.com/mpv-player/mpv/archive/v0.34.1.tar.gz"
+  sha256 "32ded8c13b6398310fa27767378193dc1db6d78b006b70dbcbd3123a1445e746"
   license :cannot_represent
   head "https://github.com/mpv-player/mpv.git", branch: "master"
 
   depends_on "docutils" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.10" => :build
+  depends_on "python@3.9" => :build
   depends_on xcode: :build
 
   depends_on "ffmpeg"
-  depends_on "libass"
   depends_on "jpeg"
   depends_on "libarchive"
+  depends_on "libass"
   depends_on "little-cms2"
   depends_on "luajit-openresty"
   depends_on "mujs"
-  depends_on "rubberband"
   depends_on "uchardet"
   depends_on "vapoursynth"
-  depends_on "zimg"
-
-  depends_on "yt-dlp" => :recommended
-  depends_on "subliminal" => :recommended
-
-  depends_on "jack" => :optional
-  depends_on "libaacs" => :optional
-  depends_on "libbluray" => :optional
-  depends_on "libcaca" => :optional
-  depends_on "libcdio" => :optional
-  depends_on "libdvdnav" => :optional
-  depends_on "libdvdread" => :optional
-  depends_on "sdl2" => :optional
-
-  on_macos do
-    depends_on "coreutils" => :recommended
-    depends_on "dockutil" => :recommended
-    depends_on "tag" => :recommended
-    depends_on "trash" => :recommended
-  end
+  depends_on "yt-dlp"
 
   def install
     opts  = Hardware::CPU.arm? ? "-mcpu=native " : "-march=native -mtune=native "
@@ -53,7 +33,7 @@ class Mpv < Formula
     ENV.append "OBJCFLAGS",   opts
     ENV.append "OBJCXXFLAGS", opts
     ENV.append "LDFLAGS",     opts + " -dead_strip"
-
+    
     # LANG is unset by default on macOS and causes issues when calling getlocale
     # or getdefaultlocale in docutils. Force the default c/posix locale since
     # that's good enough for building the manpage.
@@ -67,26 +47,26 @@ class Mpv < Formula
 
     args = %W[
       --prefix=#{prefix}
-      --confdir=#{etc}/mpv
-      --datadir=#{pkgshare}
-      --docdir=#{doc}
-      --mandir=#{man}
-      --zshdir=#{zsh_completion}
-
       --disable-html-build
       --enable-libmpv-shared
+      --enable-lua
+      --confdir=#{etc}/mpv
+      --datadir=#{pkgshare}
+      --mandir=#{man}
+      --docdir=#{doc}
+      --zshdir=#{zsh_completion}
+      --lua=luajit
     ]
-    args << "--swift-flags=-O -wmo"
 
-    system Formula["python@3.10"].opt_bin/"python3", "bootstrap.py"
-    system Formula["python@3.10"].opt_bin/"python3", "waf", "configure", *args
-    system Formula["python@3.10"].opt_bin/"python3", "waf", "install"
-    system Formula["python@3.10"].opt_bin/"python3", "TOOLS/osxbundle.py", "build/mpv"
+    system Formula["python@3.9"].opt_bin/"python3", "bootstrap.py"
+    system Formula["python@3.9"].opt_bin/"python3", "waf", "configure", *args
+    system Formula["python@3.9"].opt_bin/"python3", "waf", "install"
+    system Formula["python@3.9"].opt_bin/"python3", "TOOLS/osxbundle.py", "build/mpv"
     prefix.install "build/mpv.app"
   end
 
   test do
-    system bin/"mpv", "--ao=null", test_fixtures("test.wav")
+    system bin/"mpv", "--ao=null", "--vo=null", test_fixtures("test.wav")
     assert_match "vapoursynth", shell_output(bin/"mpv --vf=help")
   end
 end
